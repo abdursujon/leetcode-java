a1e80d6206b7c73562044f7b17302a7879ae904f
Sujon <abdur.sujon.dev@gmail.com>
2026-07-05 19:28:22 +0100

    hashing implementation in progress

--- problem-patterns/hashing/HashingTemplate.java
+++ problem-patterns/hashing/HashingTemplate.java
@@ -1,10 +1,15 @@
 package hashing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class HashingTemplate {
+
     // Pattern 1: Majority counting
     public static int findMajorityElement(int[] nums){
         Map<Integer, Integer> map = new HashMap<>();
@@ -21,6 +26,7 @@ public class HashingTemplate {
         return -1;
     }
 
+
     // Pattern 2: Find first unique character index
     public static int firstUniqueCharacter(String s){
         Map<Character, Integer> map = new HashMap<>();
@@ -35,6 +41,7 @@ public class HashingTemplate {
         return -1;
     }
 
+
     // Pattern 3: Complement look up (Two Sum) , find the index of two elements that add up to target
     public static int[] twoSum(int[] nums, int target){
         Map<Integer, Integer> map = new HashMap<>();
@@ -48,6 +55,50 @@ public class HashingTemplate {
         return new int[]{};
     }
 
+    // Pattern 4: Grouping, take array of strings and group similar element together (for example anagram)
+    public static List<List<String>> groupSimilarElements(String[] strs){
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String sorted = new String(ch);
+            map.putIfAbsent(sorted, new ArrayList<>());
+            // adds s into the arraylist
+            map.get(sorted).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    // Pattern 5: Membership and check if duplicate
+    public static boolean isDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int n: nums){
+            if(set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+
+    // Pattern 6: Longest consecutive sequence
+    public static int longestConsecutive(int[] nums){
+        // converts the nums array to first stream of int, then boxed into Integer object, then make that integer array list to list, then that list is turn into Hashset
+        Set<Integer> set = new HashSet<>(Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new)));
+        int maxLen = 0;
+        for(int n: set){
+            if(!set.contains(n - 1)){
+                int currentNum = n;
+                int currentLen = 1;
+                while(set.contains(currentNum + 1)){
+                    currentNum++;
+                    currentLen++;
+                }
+                maxLen = Math.max(maxLen, currentLen);
+            }
+        }
+
+        return maxLen;
+    }
+
     public static void main(String[] args){
         System.out.println(findMajorityElement(new int[] {1, 9, 2, 343, 4, 9, 4, 4, 2, 3, 4, 4, 4, 4, 4, 4, 4})); // 4
 
@@ -57,5 +108,10 @@ public class HashingTemplate {
 
         System.out.println(Arrays.toString(twoSum(new int[]{2, 89, 2, 5, 3, 0, 4}, 4))); // [0, 2]
         System.out.println(Arrays.toString(twoSum(new int[]{2, 89, 2, 5, 3, 0, 4}, 99))); // [] nothing adds up to 99
+
+        System.out.println(groupSimilarElements(new String[]{"eat", "tea", "mum", "tan", "ant", "bat", "ate"}));
+
+        System.out.println(longestConsecutive(new int[]{1, 6, 7, 88, 2, 3, 4, 5, 0, 100, 290}));
+        System.out.println(longestConsecutive(new int[]{88, 88, 88, 88}));
     }
 }
